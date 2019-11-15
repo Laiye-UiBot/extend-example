b47df3e2b4fcfad9426807a521d022b3cd023635
Rui Chu <churui@qq.com>
2019-11-15 16:25:36 +0800

    initial commit for java plugin

---
+++ java/JavaPlugin.java
@@ -0,0 +1,29 @@
+// UiBot内置了org.json.*，可以直接导入
+import org.json.*;
+
+public class JavaPlugin
+{
+	protected void CanNotUse()
+	{	//protected或private属性的函数，不能被UiBot调用，但可以在其他函数中调用
+		System.out.println("Cannot use this function");
+	}
+	
+	public String GetString()
+	{	//可以直接调用UiBot.API.GetString，这是UiBot提供的插件API
+		return UiBot.API.GetString("Excel/SaveBook");
+	}
+	
+	public JSONArray Concat(JSONArray array1, JSONArray array2)
+	{	//如果要传入或返回数组，请用org.json.JSONArray类型
+		JSONArray result = new JSONArray();
+		for (int i = 0; i < array1.length(); i++) 
+		{
+				result.put(array1.get(i));
+		}
+		for (int i = 0; i < array2.length(); i++) 
+		{
+				result.put(array2.get(i));
+		}
+		return result;
+	}
+}
